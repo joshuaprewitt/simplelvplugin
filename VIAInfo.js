59d10af0d34bba26d3d59642081eecf04f10ab05
joshuaprewitt <joshua.prewitt@ni.com>
2016-07-22 13:19:52 +0000

    Skyline LV Plugin Example

---
+++ VIAInfo.js
@@ -0,0 +1,17 @@
+//  via info  
+NationalInstruments.HtmlVI.viName = '_%46unction%2Egvi';
+
+//inputs: 
+NationalInstruments.HtmlVI.inputDataItem = {};
+NationalInstruments.HtmlVI.inputDataItem['di_39']='Boolean';
+NationalInstruments.HtmlVI.inputDataItem['di_46']='Boolean';
+
+//outputs:
+NationalInstruments.HtmlVI.outputDataItem = {};
+NationalInstruments.HtmlVI.outputDataItem['di_608']='String';
+NationalInstruments.HtmlVI.outputDataItem['di_26']='Array';
+NationalInstruments.HtmlVI.outputDataItem['di_160']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_154']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_148']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_626']='Boolean';
+NationalInstruments.HtmlVI.outputDataItem['di_627']='Boolean';
