9489337f620ac234aadc18e56f2cc307e927cda7
joshuaprewitt <joshua.prewitt@ni.com>
2016-07-24 04:57:52 +0000

    Updated for cRIO Temp Logger

--- VIAInfo.js
+++ VIAInfo.js
@@ -4,14 +4,12 @@ NationalInstruments.HtmlVI.viName = '_%46unction%2Egvi';
 //inputs: 
 NationalInstruments.HtmlVI.inputDataItem = {};
 NationalInstruments.HtmlVI.inputDataItem['di_39']='Boolean';
-NationalInstruments.HtmlVI.inputDataItem['di_46']='Boolean';
 
 //outputs:
 NationalInstruments.HtmlVI.outputDataItem = {};
-NationalInstruments.HtmlVI.outputDataItem['di_608']='String';
-NationalInstruments.HtmlVI.outputDataItem['di_26']='Array';
-NationalInstruments.HtmlVI.outputDataItem['di_160']='Number';
-NationalInstruments.HtmlVI.outputDataItem['di_154']='Number';
-NationalInstruments.HtmlVI.outputDataItem['di_148']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_780']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_779']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_772']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_765']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_702']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_626']='Boolean';
-NationalInstruments.HtmlVI.outputDataItem['di_627']='Boolean';
